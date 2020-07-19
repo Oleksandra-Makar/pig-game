f3cacc8d692708e45d554654dd6806eb356c847e
Oleksandra Makar <58185199+Oleksandra-Makar@users.noreply.github.com>
2020-07-19 16:43:04 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,45 +1,28 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
 let scores, roundScore, activePlayer, gamePlaying;
 
 init ();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-    if(gamePlaying){// 1. Random number
+    if(gamePlaying){
         let dice = Math.floor(Math.random() * 6) + 1;
-        //2. Display the result 
         let diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
         diceDOM.src = 'img/dice-' + dice + '.png';
 
-        //3. Update the round score IF the rolled number was NOT a 1
         if(dice !== 1){
-            //add score
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         }else{
-            //Next player
             nextPlayer();
     }
     }
 });
-//Click btn HOLD (save round score)
 document.querySelector('.btn-hold').addEventListener('click', function() {
    if(gamePlaying){
-    //add current score to global score 
     scores[activePlayer] += roundScore;
-    //update the UI 
+       
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
-    //Check if player won the game 
     if(scores[activePlayer] >= 100){
         document.querySelector('#name-' + activePlayer).textContent = 'Winner';
         document.querySelector('.dice').style.display = 'none';
@@ -52,7 +35,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     }
    }
 });
-//switching between palyers 
+
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
@@ -65,7 +48,7 @@ function nextPlayer() {
         
     document.querySelector('.dice').style.display = 'none';
 };
-//Start new game
+
 document.querySelector('.btn-new').addEventListener('click', init);
 
 function init () {
